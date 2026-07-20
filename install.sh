279b4eb1a4d56adfcbac2354ef33d9cff292c531
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2026-07-19 20:09:04 -0700

    fix: correct conditional syntax in install script for architecture checks

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 
 arch=$(uname -m)
-if [ "$arch" == "armv7l" ]; then
+if [ "$arch" = "armv7l" ]; then
     cp -pvr ./mymonitor-rpi3b /usr/local/bin/mymonitor-rpi3
     cp -pvr ./mymonitorrpi3.service /etc/systemd/system/mymonitorrpi3.service
     systemctl daemon-reload
@@ -8,7 +8,7 @@ if [ "$arch" == "armv7l" ]; then
     systemctl start mymonitorrpi3.service
 fi
 
-if [ "$arch" == "aarch64" -o "$arch" == "x86_64" ]; then
+if [ "$arch" = "aarch64" ] || [ "$arch" = "x86_64" ]; then
     cp -pvr ./mymonitor-rpi4 /usr/local/bin/mymonitor-rpi4
     cp -pvr ./mymonitorrpi4.service /etc/systemd/system/mymonitorrpi4.service
     systemctl daemon-reload
